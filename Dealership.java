3f29bfefb8e0ad0d58632c9fbecbecb1f122ed24
Walkerwalt <walker.burden@stu.daviess.kyschools.us>
2026-03-04 00:14:37 +0000

    C1

--- Dealership.java
+++ Dealership.java
@@ -5,9 +5,25 @@ public class Dealership {
         ArrayList<Vehicle> inventory = new ArrayList<Vehicle>();
 
         // TODO: Add a standard Vehicle and an ElectricCar
+        inventory.add(new Vehicle("F150"));
+        inventory.add(new ElectricCar("Telsa"));
         
+        inventory.get(1).drive(400);
+        
+        System.out.println(((ElectricCar)inventory.get(1)).getBattery());
+
         System.out.println("--- End of Day Report ---");
         // TODO: Loop through inventory. 
         // TODO: If a vehicle is an ElectricCar, check if it needs a charge.
+        for(Vehicle car: inventory)
+        {
+            System.out.println(car);
+            if (car instanceof ElectricCar){
+                if (((ElectricCar)car).needsCharge()){
+                    ((ElectricCar)car).charge();
+                    System.out.println(car + "\n" + ((ElectricCar)car).getBattery());
+                }
+            }
+        }
     }
 }
--- ElectricCar.java
+++ ElectricCar.java
@@ -4,6 +4,8 @@ public class ElectricCar extends Vehicle {
     public ElectricCar(String model) {
         // TODO: Use super() to set the model
         // TODO: Set batteryPercent to 100.0
+        super(model);
+        batteryPercent = 100;
     }
 
     /**
@@ -13,8 +15,17 @@ public class ElectricCar extends Vehicle {
     public void drive(int distance) {
         // TODO: Call super.drive() 
         // TODO: Calculate battery loss
+        super.drive(distance);
+        batteryPercent -= (double)distance/5;
     }
 
+    public boolean needsCharge(){
+        return batteryPercent < 25;
+    }
+
+    public double getBattery(){
+        return batteryPercent;
+    }
     public void charge() {
         batteryPercent = 100.0;
     }
